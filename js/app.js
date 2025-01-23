3f20e2ac7c20d7533dfeb34935fb9dc1d1fd6e63
meti-0763 <daramade4@gmail.com>
2025-01-23 08:29:54 +0330

    sec2

--- js/app.js
+++ js/app.js
@@ -9,4 +9,5 @@ $.querySelector("#lightToggle") .addEventListener(`click`,e =>{
 
 $.querySelector("#ham-menu") .addEventListener(`click`,e =>{
  $.querySelector(`#navbar-cta`).classList.toggle("hidden")   
+ $.querySelector(`.fa-sun`).classList.toggle("fa-moon")   
 })
