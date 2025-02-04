f715b3edbe14ab5c406e83cbf513523d4e1cd6ec
meti-0763 <daramade4@gmail.com>
2025-02-04 22:19:03 +0330

    demo-2

--- js/app.js
+++ js/app.js
@@ -4,10 +4,20 @@ let $ = document
 
 
 
-$.querySelector("#lightToggle") .addEventListener(`click`,e =>{
- $.querySelector(`html`).classList.toggle("dark")   
- $.querySelector(`.fa-sun`).classList.toggle("fa-moon")   
+$.querySelectorAll(".lightToggle").forEach(element => {
+  
+  element.addEventListener(`click`,e =>{
+  
+  
+  
+  
+  
+      $.querySelector(`html`).classList.toggle("dark")   
+      $.querySelector(`.fa-sun`).classList.toggle("fa-moon")   
+    })
 })
+
+
 
 $.querySelector("#ham-menu") .addEventListener(`click`,e =>{
  $.querySelector(`#navbar-cta`).classList.toggle("hidden")   
